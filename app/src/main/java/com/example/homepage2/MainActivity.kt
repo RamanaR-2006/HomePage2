c8bffb98432a8a084c18b6f5d9390cf6b7b6f3a3
RamanaR-2006 <ramanaramachandran14@gmail.com>
2023-08-13 00:49:54 +0530

    stuff

--- app/src/main/java/com/example/homepage2/MainActivity.kt
+++ app/src/main/java/com/example/homepage2/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
@@ -33,6 +34,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var minusButton: ImageButton
     private lateinit var plusButton: ImageButton
 
+    private var selectedEquipmentCount = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -44,47 +46,19 @@ class MainActivity : AppCompatActivity() {
             window.statusBarColor = resources.getColor(R.color.your_status_bar_color, theme)
         }
 
-        quantityTextView = dialogBinding.quantityTextView
-        minusButton = dialogBinding.minusButton
-        plusButton = dialogBinding.plusButton
-
-        // Set click listeners for minus and plus buttons
-        minusButton.setOnClickListener {
-            val currentQuantity = quantityTextView.text.toString().toInt()
-            if (currentQuantity > 0) {
-                quantityTextView.text = (currentQuantity - 1).toString()
-            }
-        }
-
-        plusButton.setOnClickListener {
-            val currentQuantity = quantityTextView.text.toString().toInt()
-            quantityTextView.text = (currentQuantity + 1).toString()
-        }
-
         val spinner: Spinner = findViewById(R.id.spinner)
-        var SelectedItems = 0
-        val items = listOf("Number of selected items is ${SelectedItems}")
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        val items = listOf("--Select--", "1")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, listOf(getSpinnerText()))
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
-        spinner.setOnClickListener {
-            showDialogBox()
-            SelectedItems = quantityTextView.text.toString().toInt()
-            updateSpinnerSelectionState()
 
-        }
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                if (position > 0) {
-                    showDialogBox()
-                }
-                updateSpinnerSelectionState()
+        spinner.setOnTouchListener { _, motionEvent ->
+            if (motionEvent.action == MotionEvent.ACTION_UP) {
+                showDialogBox()
             }
+            true
+        }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                // Not needed in this case
-            }
-        }
         val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
         recyclerview.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
@@ -108,14 +82,28 @@ class MainActivity : AppCompatActivity() {
 
         val adapter2 = CustomAdapter2(data1)
         recyclerview1.adapter = adapter2
-        val isRecyclerViewSelected1 = (recyclerview.adapter as? CustomAdapter)?.isItemSelected == true ||
-                (recyclerview1.adapter as? CustomAdapter2)?.isItemSelected == true
+
         dialogBinding = DialogBoxLayoutBinding.inflate(LayoutInflater.from(this))
         dialog = AlertDialog.Builder(this)
             .setView(dialogBinding.root)
             .create()
 
+        quantityTextView = dialogBinding.quantityTextView
+        minusButton = dialogBinding.minusButton
+        plusButton = dialogBinding.plusButton
 
+        // Set click listeners for minus and plus buttons
+        minusButton.setOnClickListener {
+            val currentQuantity = quantityTextView.text.toString().toInt()
+            if (currentQuantity > 0) {
+                quantityTextView.text = (currentQuantity - 1).toString()
+            }
+        }
+
+        plusButton.setOnClickListener {
+            val currentQuantity = quantityTextView.text.toString().toInt()
+            quantityTextView.text = (currentQuantity + 1).toString()
+        }
     }
 
     fun showStartDatePicker(view: View) {
@@ -158,6 +146,13 @@ class MainActivity : AppCompatActivity() {
         datePickerDialog.show()
     }
 
+    private fun updateSpinnerText() {
+        val spinner: Spinner = findViewById(R.id.spinner)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, listOf(getSpinnerText()))
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+    }
+
     private fun updateSpinnerSelectionState() {
         val spinner = findViewById<Spinner>(R.id.spinner)
         val selectedItemPosition = spinner.selectedItemPosition
@@ -170,13 +165,11 @@ class MainActivity : AppCompatActivity() {
         val finalButton = findViewById<ImageButton>(R.id.finalButton)
         if (areAllFieldsFilled()) {
             finalButton.setBackgroundColor(ContextCompat.getColor(this, R.color.your_desired_color))
-            Toast.makeText(this,"Valid!!",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Valid!!", Toast.LENGTH_SHORT).show()
         } else {
             finalButton.setBackgroundColor(ContextCompat.getColor(this, R.color.default_button_color))
         }
     }
-
-
 
     private fun areAllFieldsFilled(): Boolean {
         val locationEditText = findViewById<EditText>(R.id.location)
@@ -195,6 +188,11 @@ class MainActivity : AppCompatActivity() {
                 isRecyclerViewSelected
 
     }
+
+    private fun getSpinnerText(): String {
+        return "Number of selected items: $selectedEquipmentCount"
+    }
+
     private fun showDialogBox() {
         // Clear previous quantity value
         dialogBinding.quantityTextView.text = "0"
@@ -202,5 +200,4 @@ class MainActivity : AppCompatActivity() {
         // Show the dialog box
         dialog.show()
     }
-
 }
